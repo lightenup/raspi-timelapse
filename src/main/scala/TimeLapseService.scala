58d618f7ba01227214fdafef7dd7226077a83928
Andreas Jacobsen <jacobsen.andreas@gmail.com>
2014-03-23 14:35:27 +0100

    Basic setup

---
+++ src/main/scala/TimeLapseService.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by andreas on 16/03/14.
+ */
+object TimeLapseService {
+  def main(args: Array[String]) {
+    val camera = new Camera()
+    val picture = camera.takePicture()
+  }
+
+}
